13decc36877544f79868f4630904e17657b405e6
Srijaswi-N <137861761+Srijaswi-N@users.noreply.github.com>
2023-07-30 19:25:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+
+import Calci from './Calculater/Calci';
+import './App.css';
+function App() {
+  return (
+      <div className='App' >
+      <Calci />
+    </div>
+  );
+}
+
+export default App;
